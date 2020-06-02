7c529f1cafbb0ce18329415838dc4a1023186281
vovak2018 <48640784+vovak2018@users.noreply.github.com>
2020-06-02 12:23:11 +0300

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,6 +1,6 @@
 "use strict";
 document.write('<a class="a1" href="https://vovak2018.github.io/" name="top">')
 document.write('    <header>')
-document.write('        <h1>Азимут Мытищи</h1>')
+document.write('        <h1 style="color: red;">!!!Этот проект будет закрыт в ближайшее время!!!</h1>')
 document.write('    </header>')
 document.write('</a>')
