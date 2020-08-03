d38767da438aba4f88e94f9134be7793652a4482
vovak2018 <48640784+vovak2018@users.noreply.github.com>
2020-08-03 22:33:11 +0300

    Update mobileMenu.js

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -2,5 +2,5 @@ document.write('<div class="menumobile">');
 document.write('<div class="menuli"><a href="https://vovak2018.github.io/"><i class="fa fa-home" aria-hidden="true"></i></a></div>');
 document.write('<div class="menuli"><a href="https://vovak2018.github.io/information.html"><i class="fa fa-info" aria-hidden="true"></i></a></div>');
 document.write('<div class="menuli"><a href="https://vovak2018.github.io/kontacts.html"><i class="fa fa-address-card" aria-hidden="true"></i></a></div>');
-document.write('<div class="menuli"><a href="https://vovak2018.github.io/archive.html"><i class="fa fa-file-archive" aria-hidden="true"></i></a></div>');
+document.write('<div class="menuli"><a href="https://vovak2018.github.io/shop.html"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a></div>');
 document.write('</div>');
