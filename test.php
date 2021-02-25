67e6f56c19c1ff5e3901c8749b41c5a525b8eb0b
lava_frai <48640784+vovak2018@users.noreply.github.com>
2021-02-25 20:01:56 +0300

    Update and rename test.html to test.php

---
+++ test.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Document</title>
+  </head>
+  <body>
+    <?php
+      echo "Test text\n<br>";
+      echo (1 + 10);
+      $a = 10;
+      echo "<br>";
+      echo $a + 228;
+    ?>
+  </body>
+</html>
